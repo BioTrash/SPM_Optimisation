4203ffee96c7e82975ed05ed6247e4280210a972
Rufus Trukhin <rufustrukhin@gmail.com>
2024-04-25 18:57:31 +0200

    Chunks Implemented

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -9,7 +9,9 @@ public class WorldGenerator : MonoBehaviour
 {
     public static WorldGenerator SharedInstance;
     public List<GameObject> pooledObjects;
+    public List<GameObject> obstaclePool;
     public HashSet<Vector2> noiseList;
+    public Dictionary<int, HashSet<GameObject>> chunkPool;
     
     [Header("World")]
     public int width = 200;
@@ -17,7 +19,8 @@ public class WorldGenerator : MonoBehaviour
     public float percentageBlocks = 0.35f;
     public float noiseScale = 15.0f;
     private int fixedFrameCounter = 0;
-    public float sample = 0.0f;
+    private float sample = 0.0f;
+    public int chunkSize = 25;
 
     // Frequency at which to call the function
     public int callFrequency = 60;
@@ -39,12 +42,52 @@ public class WorldGenerator : MonoBehaviour
     void Awake()
     {
         noiseList = new HashSet<Vector2>();
+        pooledObjects = new List<GameObject>();
+        chunkPool = new Dictionary<int, HashSet<GameObject>>();
         SharedInstance = this;
     }
 
     void Start()
-    {   
-        pooledObjects = new List<GameObject>();
+    {
+        HashSet<GameObject> tempSet = new HashSet<GameObject>();
+        int currentChunkIndex = 1; // Start with key 1
+        
+        // Iterate through the obstaclePool and group elements into chunks
+        foreach (GameObject obstacle in obstaclePool)
+        {
+            // Calculate the current chunk index based on obstacle position
+            int chunkX = (int)(obstacle.transform.position.x / chunkSize) + 1;
+            int chunkZ = (int)(obstacle.transform.position.z / chunkSize) + 1;
+            int chunkKey = (chunkZ - 1) * (width / chunkSize) + chunkX;
+
+            // Add obstacle to the corresponding chunk
+            if (!chunkPool.ContainsKey(chunkKey))
+            {
+                chunkPool[chunkKey] = new HashSet<GameObject>();
+            }
+
+            chunkPool[chunkKey].Add(obstacle);
+        }
+
+        Debug.Log(chunkPool.Count);
+        
+        // Retrieve all elements with the same key
+        HashSet<GameObject> retrievedElements = chunkPool[1];
+
+        // Do something with the retrieved elements
+        foreach (GameObject element in retrievedElements)
+        {
+           element.SetActive(false);
+        }
+        
+        HashSet<GameObject> retrievedElements6 = chunkPool[6];
+
+        // Do something with the retrieved elements
+        foreach (GameObject element in retrievedElements6)
+        {
+            element.SetActive(false);
+        }
+        
         GameObject tmp;
         for (int i = 0; i < numAI; i++)
         {
@@ -64,6 +107,7 @@ public class WorldGenerator : MonoBehaviour
                         if (sample < percentageBlocks)
                         {
                             noiseList.Add(new Vector2(x, y));
+                            
                         }
                     }
                 }
@@ -73,11 +117,6 @@ public class WorldGenerator : MonoBehaviour
             tmp.SetActive(false);
             tmp.GetComponent<EnemyPawn>().sharedInstance = SharedInstance;
             pooledObjects.Add(tmp);
-        }
-        
-        foreach (Vector2 obstacle in noiseList)
-        {
-            Debug.Log(obstacle);
         }
         
         PlaceAI();
@@ -120,10 +159,21 @@ public class WorldGenerator : MonoBehaviour
     [ContextMenu("Generate New World")]
     public void BuildWorld()
     {
+        /*
+         * Generate the entire map
+         * Split the map in 25x25 chunks (make size a public variable)
+         * Put all elements in a Map, each chunk is a key (int) while each element is a HashSet<GameObject>
+         * SetActive(true) on chunk with the player as well as all directly adjacent chunks, including diagonally
+         * Everytime player moves check whether chunk has changed
+         * SetActive(false) on all chunks that are no longer directly adjacent to chunk with the player
+         */
+        
+        
         // Log execution time.
         var stopwatch = new System.Diagnostics.Stopwatch();
         stopwatch.Start();
 
+        obstaclePool = new List<GameObject>();
         // NOTE: Clean up previous world so we don't double up.
         while (transform.childCount > 0)
         {
@@ -140,11 +190,22 @@ public class WorldGenerator : MonoBehaviour
         plane.GetComponent<Renderer>().material = planeMaterial;
         plane.transform.SetParent(transform);
 
+        int completedColumns = 0;
+
         // Create obstacles/geometry.
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
+                if (x >= chunkSize)
+                {
+                    x += chunkSize * completedColumns;
+                    if (y >= height)
+                    {
+                        y = 0;
+                        completedColumns++;
+                    }
+                }
                 sample = 0.0f;
                 // NOTE: Add a solid border and generate inside with perlin noise.
                 if (x > 0 && y > 0 && x < width - 1 && y < height - 1)
@@ -155,7 +216,6 @@ public class WorldGenerator : MonoBehaviour
 
                 if (sample < percentageBlocks)
                 {
-                    //noiseList.Add(new Vector2(x, y)); //adds an obstacle placement vector2 in the list
                     // NOTE: Larger noice gives higher blocks.
                     float obstacleHeight = 3.0f - sample * 2.0f;
                     GameObject obstacle = GameObject.CreatePrimitive(PrimitiveType.Cube);
@@ -163,6 +223,8 @@ public class WorldGenerator : MonoBehaviour
                     obstacle.transform.localScale = new Vector3(1.0f, obstacleHeight, 1.0f);
                     obstacle.transform.SetParent(transform);
                     obstacle.GetComponent<Renderer>().material = obstacleMaterial;
+                    
+                    obstaclePool.Add(obstacle);
                 }
             }
         }
